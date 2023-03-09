ea609c1c54fe030b1af40e3ccf5b5f9ca78095fd
Rayen.hajji <rayenhajji@stud.uni-hannover.de>
2023-03-09 19:23:23 +0100

    Last changes in the formation layer before week 10 meet

--- formation_layer/include/formation_layer/formation_layer.h
+++ formation_layer/include/formation_layer/formation_layer.h
@@ -13,63 +13,77 @@ using namespace std;
 
 namespace formation_layer_namespace
 {
-
-struct Point{
-    double x;
-    double y;
-    double z;
+struct PointInt {
+    int x;
+    int y;
 };
 class FormationLayer : public costmap_2d::Layer
 {
 public :
+    using Polygon = std::vector<geometry_msgs::Point>;
     FormationLayer();
 
     virtual void onInitialize();
     virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,double* max_y);
     virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
 
+
 private :
     ros::NodeHandle nh_;
     dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
 
     //Subscribers to the robots footprints
-    ros::Subscriber robot_pose_sub_0_;
-    ros::Subscriber robot_fp_subs_0_;
-    ros::Subscriber robot_fp_subs_1_;
-    ros::Subscriber robot_fp_subs_2_;
+    // ros::Subscriber robot_pose_sub_0_;
+    // ros::Subscriber robot_fp_subs_0_;
+    // ros::Subscriber robot_fp_subs_1_;
+    // ros::Subscriber robot_fp_subs_2_;
+    ros::Subscriber formationFPSubs_;
 
     double mark_x_, mark_y_;
 
-    //polygons to save the robots footprints   
-    std::vector<geometry_msgs::Point32> fp_0;
-    std::vector<geometry_msgs::Point32> fp_1;
-    std::vector<geometry_msgs::Point32> fp_2;
-    // geometry_msgs::PolygonStamped fp_2; 
+    //polygon to save the formation footprint  
+    Polygon formation_fp;
+    
 
     void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
 
-    //callback functions of the robot footprints
-    void robotposeCallback_0(const geometry_msgs::PoseWithCovarianceStamped &msg);
-    void robotFPCallback_0(const geometry_msgs::Polygon &msg);
-    void robotFPCallback_1(const geometry_msgs::Polygon &msg);
-    void robotFPCallback_2(const geometry_msgs::Polygon &msg);
+    //callback function of the formation footprint
+    void formationFPCallback(const geometry_msgs::PolygonStamped &msg);
 
+    /// \brief             rasterizes line between two map coordinates into a set of cells
+    /// \note              since Costmap2D::raytraceLine() is based on the size_x and since we want to rasterize polygons that might also be located outside map bounds we provide a modified raytrace
+    ///                    implementation(also Bresenham) based on the integer version presented here : http : //playtechs.blogspot.de/2007/03/raytracing-on-grid.html
+    /// \param x0          line start x-coordinate (map frame)
+    /// \param y0          line start y-coordinate (map frame)
+    /// \param x1          line end x-coordinate (map frame)
+    /// \param y1          line end y-coordinate (map frame)
+    /// \param[out] cells  new cells in map coordinates are pushed back on this container
+    void linetrace(int x0, int y0, int x1, int y1, std::vector<PointInt> &cells);
     
-    // /// \brief             rasterizes line between two map coordinates into a set of cells
-    // /// \note              since Costmap2D::raytraceLine() is based on the size_x and since we want to rasterize polygons that might also be located outside map bounds we provide a modified raytrace
-    // ///                    implementation(also Bresenham) based on the integer version presented here : http : //playtechs.blogspot.de/2007/03/raytracing-on-grid.html
-    // /// \param x0          line start x-coordinate (map frame)
-    // /// \param y0          line start y-coordinate (map frame)
-    // /// \param x1          line end x-coordinate (map frame)
-    // /// \param y1          line end y-coordinate (map frame)
-    // /// \param[out] cells  new cells in map coordinates are pushed back on this container
-    // void linetrace(int x0, int y0, int x1, int y1, std::vector<PointInt> &cells);
+    /// \brief                     extracts the boundary of a polygon in terms of map cells
+    /// \note                      this method is based on Costmap2D::polygonOutlineCells() but accounts for a self - implemented raytrace algorithm and allows negative map coordinates
+    /// \param polygon             polygon defined  by a vector of map coordinates
+    /// \param[out] polygon_cells  new cells in map coordinates are pushed back on this container
+    void polygonOutlineCells(const std::vector<PointInt> &polygon, std::vector<PointInt> &polygon_cells);
     
-    // /// \brief                     extracts the boundary of a polygon in terms of map cells
-    // /// \note                      this method is based on Costmap2D::polygonOutlineCells() but accounts for a self - implemented raytrace algorithm and allows negative map coordinates
-    // /// \param polygon             polygon defined  by a vector of map coordinates
-    // /// \param[out] polygon_cells  new cells in map coordinates are pushed back on this container
-    // void polygonOutlineCells(const std::vector<PointInt> &polygon, std::vector<PointInt> &polygon_cells);
+    /// \brief                     converts polygon (in map coordinates) to a set of cells in the map
+    /// \note                      This method is mainly based on Costmap2D::convexFillCells() but accounts for a self - implemented polygonOutlineCells() method and allows negative map coordinates
+    /// \param polygon             Polygon defined  by a vector of map coordinates
+    /// \param fill                If true, the interior of the polygon will be considered as well
+    /// \param[out] polygon_cells  new cells in map coordinates are pushed back on this container
+    void rasterizePolygon(const std::vector<PointInt> &polygon, std::vector<PointInt> &polygon_cells, bool fill);
+
+    /// \brief                set cost in a Costmap2D for a polygon (polygon may be located outside bounds)
+    /// \param master_grid    reference to the Costmap2D object
+    /// \param polygon        polygon defined by a vector of points (in world coordinates)
+    /// \param cost           the cost value to be set (0,255)
+    /// \param min_i          minimum bound on the horizontal map index/coordinate
+    /// \param min_j          minimum bound on the vertical map index/coordinate
+    /// \param max_i          maximum bound on the horizontal map index/coordinate
+    /// \param max_j          maximum bound on the vertical map index/coordinate
+    /// \param fill_polygon   if true, tue cost for the interior of the polygon will be set as well
+    void setPolygonCost(costmap_2d::Costmap2D &master_grid, const Polygon &polygon,
+                        unsigned char cost, int min_i, int min_j, int max_i, int max_j, bool fill_polygon);
     };
 }
 #endif
--- formation_layer/src/formation_layer.cpp
+++ formation_layer/src/formation_layer.cpp
@@ -4,137 +4,205 @@
 
 PLUGINLIB_EXPORT_CLASS(formation_layer_namespace::FormationLayer, costmap_2d::Layer)
 
-// using costmap_2d::LETHAL_OBSTACLE; 
 using costmap_2d::FREE_SPACE;
 namespace formation_layer_namespace
 {
-    FormationLayer::FormationLayer() : nh_("~") {}
+    FormationLayer::FormationLayer() {} 
     
     void FormationLayer::onInitialize()
     {
-        ros::NodeHandle nh_("~/"+ name_);
+        ROS_INFO("FormationLayer::onInitialize");
+        this->nh_ = ros::NodeHandle("~/"+ name_);
         current_ = true;
         //Plugin initialization
-        dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh_);
+        dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(this->nh_);
         dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(&FormationLayer::reconfigureCB, this, _1, _2);
         dsrv_->setCallback(cb);
         
-        //Subscribers to the robots footprints
-        robot_pose_sub_0_= nh_.subscribe("/robot0/amcl_pose",10, &FormationLayer::robotposeCallback_0, this);
-        robot_fp_subs_0_ = nh_.subscribe("/robot0/move_base_flex/local_costmap/footprint", 10, &FormationLayer::robotFPCallback_0, this);
-        robot_fp_subs_1_ = nh_.subscribe("/robot1/move_base_flex/local_costmap/footprint", 10, &FormationLayer::robotFPCallback_1, this);
-        robot_fp_subs_2_ = nh_.subscribe("/robot2/move_base_flex/local_costmap/footprint", 10, &FormationLayer::robotFPCallback_2, this);
+
+        formationFPSubs_ = this->nh_.subscribe("/robot0/move_base_flex/formation_footprint",10, &FormationLayer::formationFPCallback, this);
     }
 
     void FormationLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level){
         enabled_ = config.enabled;
     }   
 
-    void FormationLayer::robotposeCallback_0(const geometry_msgs::PoseWithCovarianceStamped &msg){
-        ROS_INFO("PoseCallback 0 started");
-        mark_x_ = msg.pose.pose.position.x;
-        mark_y_ = msg.pose.pose.position.y;
+    void FormationLayer::formationFPCallback(const geometry_msgs::PolygonStamped &msg)
+    {
+        ROS_INFO_THROTTLE(10,"Formation Callback started");
+        for(const auto& point : msg.polygon.points){
+            geometry_msgs::Point p;
+            p.x = point.x;
+            p.y = point.y;
+            ROS_INFO_THROTTLE(10,"p=(%f,%f)",p.x,p.y);
+            formation_fp.push_back(p);
+            ROS_INFO_THROTTLE(10,"Formation Callback done"); 
+        }
+    }
+
+    void FormationLayer::linetrace(int x0, int y0, int x1, int y1, std::vector<PointInt> &cells)
+    {
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    PointInt pt;
+    pt.x = x0;
+    pt.y = y0;
+    int n = 1 + dx + dy;
+    int x_inc = (x1 > x0) ? 1 : -1;
+    int y_inc = (y1 > y0) ? 1 : -1;
+    int error = dx - dy;
+    dx *= 2;
+    dy *= 2;
+
+    for (; n > 0; --n) {
+        cells.push_back(pt);
+
+        if (error > 0) {
+            pt.x += x_inc;
+            error -= dy;
+        } else {
+            pt.y += y_inc;
+            error += dx;
+        }
+    }
+    }
+
+    void FormationLayer::polygonOutlineCells(const std::vector<PointInt> &polygon, std::vector<PointInt> &polygon_cells)
+    {
+        for (unsigned int i = 0; i < polygon.size() - 1; ++i) {
+            linetrace(polygon[i].x, polygon[i].y, polygon[i + 1].x, polygon[i + 1].y, polygon_cells);
+        }
+        if (!polygon.empty()) {
+            unsigned int last_index = polygon.size() - 1;
+            // we also need to close the polygon by going from the last point to the first
+            linetrace(polygon[last_index].x, polygon[last_index].y, polygon[0].x, polygon[0].y, polygon_cells);
+        }
+    }
+
+    void FormationLayer::rasterizePolygon(const std::vector<PointInt> &polygon, std::vector<PointInt> &polygon_cells, bool fill)
+    {
+        // this implementation is a slighly modified version of Costmap2D::convexFillCells(...)
+
+        //we need a minimum polygon of a traingle
+        if (polygon.size() < 3)
+            return;
+
+        //first get the cells that make up the outline of the polygon
+        polygonOutlineCells(polygon, polygon_cells);
+
+        if (!fill)
+            return;
+
+        //quick bubble sort to sort points by x
+        PointInt swap;
+        unsigned int i = 0;
+        while (i < polygon_cells.size() - 1) {
+            if (polygon_cells[i].x > polygon_cells[i + 1].x) {
+                swap = polygon_cells[i];
+                polygon_cells[i] = polygon_cells[i + 1];
+                polygon_cells[i + 1] = swap;
+
+                if (i > 0)
+                    --i;
+            } else
+                ++i;
+        }
+
+        i = 0;
+        PointInt min_pt;
+        PointInt max_pt;
+        int min_x = polygon_cells[0].x;
+        int max_x = polygon_cells[(int)polygon_cells.size() - 1].x;
+
+        //walk through each column and mark cells inside the polygon
+        for (int x = min_x; x <= max_x; ++x) {
+            if (i >= (int)polygon_cells.size() - 1)
+                break;
+
+            if (polygon_cells[i].y < polygon_cells[i + 1].y) {
+                min_pt = polygon_cells[i];
+                max_pt = polygon_cells[i + 1];
+            } else {
+                min_pt = polygon_cells[i + 1];
+                max_pt = polygon_cells[i];
+            }
+
+            i += 2;
+            while (i < polygon_cells.size() && polygon_cells[i].x == x) {
+                if (polygon_cells[i].y < min_pt.y)
+                    min_pt = polygon_cells[i];
+                else if (polygon_cells[i].y > max_pt.y)
+                    max_pt = polygon_cells[i];
+                ++i;
+            }
+
+            PointInt pt;
+            //loop though cells in the column
+            for (int y = min_pt.y; y < max_pt.y; ++y) {
+                pt.x = x;
+                pt.y = y;
+                polygon_cells.push_back(pt);
+            }
+        }
     }
     
-    void FormationLayer::robotFPCallback_0(const geometry_msgs::Polygon &msg)
-    {
-        ROS_INFO("Callback 0 started");
-        for(const auto& point : msg.points){
-            geometry_msgs::Point32 p;
-            p.x = point.x;
-            p.y = point.y;
-            p.z = 0;
-            fp_0.push_back(p);
-            ROS_INFO("Callback 0 done"); 
-        }
-    }
-
-    void FormationLayer::robotFPCallback_1(const geometry_msgs::Polygon &msg){
-        ROS_INFO("Callback 1 started");
-        for(const auto& point : msg.points){
-            geometry_msgs::Point32 p;
-            p.x = point.x;
-            p.y = point.y;
-            p.z = 0;
-            fp_1.push_back(p);
-            ROS_INFO("Callback 1 done"); 
-        }
-    }
-
-    void FormationLayer::robotFPCallback_2(const geometry_msgs::Polygon &msg){
-        ROS_INFO("Callback 2 started");
-        for(const auto& point : msg.points){
-            geometry_msgs::Point32 p;
-            p.x = point.x;
-            p.y = point.y;
-            p.z = 0;
-            fp_1.push_back(p);
-            ROS_INFO("Callback 2 done"); 
-        }
-    }
-
-    // void AreaLayer::linetrace(int x0, int y0, int x1, int y1, std::vector<PointInt> &cells)
-    // {
-    //     int dx = abs(x1 - x0);
-    //     int dy = abs(y1 - y0);
-    //     PointInt pt;
-    //     pt.x = x0;
-    //     pt.y = y0;
-    //     int n = 1 + dx + dy;
-    //     int x_inc = (x1 > x0) ? 1 : -1;
-    //     int y_inc = (y1 > y0) ? 1 : -1;
-    //     int error = dx - dy;
-    //     dx *= 2;
-    //     dy *= 2;
-    //     for (; n > 0; --n) {
-    //         cells.push_back(pt);
-    //         if (error > 0.0) {
-    //             pt.x += x_inc;
-    //             error -= dy;
-    //         } else {
-    //             pt.y += y_inc;
-    //             error += dx;
-    //         }
-    //     }
-    // }
-
-    // void AreaLayer::polygonOutlineCells(const std::vector<PointInt> &polygon, std::vector<PointInt> &polygon_cells)
-    // {
-    //     for (unsigned int i = 0; i < polygon.size() - 1; ++i) {
-    //         linetrace(polygon[i].x, polygon[i].y, polygon[i + 1].x, polygon[i + 1].y, polygon_cells);
-    //     }
-    //     if (!polygon.empty()) {
-    //         unsigned int last_index = polygon.size() - 1;
-    //         // we also need to close the polygon by going from the last point to the first
-    //         linetrace(polygon[last_index].x, polygon[last_index].y, polygon[0].x, polygon[0].y, polygon_cells);
-    //     }
-    // }
-
     void FormationLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x, double* max_y)
     {
         if(!enabled_)
             return;
-
-        // mark_x_ = robot_x;
-        // mark_y_ = robot_y;
+        ROS_INFO_THROTTLE(10,"UpdateBounds started");
+        mark_x_ = robot_x;
+        mark_y_ = robot_y;
 
         *min_x = std::min(*min_x, mark_x_);
         *min_y = std::min(*min_y, mark_y_);
         *max_x = std::max(*max_x, mark_x_);
         *max_y = std::max(*max_y, mark_y_);
-        ROS_INFO("UpdateBounds done");
+        ROS_INFO_THROTTLE(10,"UpdateBounds done");
     }
 
+    void FormationLayer::setPolygonCost(costmap_2d::Costmap2D &master_grid, const Polygon &polygon,
+                        unsigned char cost, int min_i, int min_j, int max_i, int max_j, bool fill_polygon)
+    {   
+        std::vector<PointInt> map_polygon;
+        for (unsigned int i = 0; i < polygon.size(); ++i) {
+            PointInt loc;
+            master_grid.worldToMapNoBounds(polygon[i].x, polygon[i].y, loc.x, loc.y);
+            map_polygon.push_back(loc);
+        }
+
+        std::vector<PointInt> polygon_cells;
+
+        // get the cells that fill the polygon
+        rasterizePolygon(map_polygon, polygon_cells, fill_polygon);
+
+        // set the cost of those cells
+        for (unsigned int i = 0; i < polygon_cells.size(); ++i) {
+            int mx = polygon_cells[i].x;
+            int my = polygon_cells[i].y;
+            // check if point is outside bounds
+            if (mx < min_i || mx >= max_i)
+                continue;
+            if (my < min_j || my >= max_j)
+                continue;
+            master_grid.setCost(mx, my, cost);
+        }
+    }
     void FormationLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j)
     {
+        ROS_INFO_THROTTLE(10,"UpdateCosts started");
         if(!enabled_)
             return;
-        unsigned int mx;
-        unsigned int my;
-        if(master_grid.worldToMap(mark_x_, mark_y_, mx, my)){
-            //  master_grid.setCost(mx, my, LETHAL_OBSTACLE);
-             master_grid.setCost(mx, my, FREE_SPACE);
-             ROS_INFO("SetCost done");
+        for (int i = 0; i < formation_fp.size(); ++i) {
+        setPolygonCost(master_grid, formation_fp, FREE_SPACE, min_i, min_j, max_i, max_j, true);
         }
-    }//end_namespace
-}
+        ROS_INFO_THROTTLE(10,"UpdateCosts done");
+        // unsigned int mx;
+        // unsigned int my;
+        // if(master_grid.worldToMap(mark_x_, mark_y_, mx, my)){
+        //     //  master_grid.setCost(mx, my, LETHAL_OBSTACLE);
+        //      master_grid.setCost(mx, my, FREE_SPACE);
+        //      ROS_INFO_THROTTLE(5,"SetCost done");
+        // }
+    }
+}//end_namespace
